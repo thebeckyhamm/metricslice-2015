a174f13905c7a209e91f05ec5b637b5160ae921b
Becky Hamm <becky@metricslice.com>
2015-05-15 10:40:58 -0400

    replace gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,90 +1,18 @@
-var gulp            = require('gulp'),
-    sass            = require('gulp-sass'),
-    autoprefixer    = require('gulp-autoprefixer'),
-    minifycss       = require('gulp-minify-css'),
-    uglify          = require('gulp-uglify'),
-    rename          = require('gulp-rename'),
-    concat          = require('gulp-concat'),
-    notify          = require('gulp-notify'),
-    cache           = require('gulp-cache'),
-    plumber         = require('gulp-plumber'),
-    browserSync     = require('browser-sync'),
-    critical        = require('critical'),
-    svgSprite       = require('gulp-svg-sprite'),
-    cp              = require('child_process');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var cp          = require('child_process');
 
-
-    config                  = {
-        shape               : {
-            dimension       : {         // Set maximum dimensions
-                maxWidth    : 64,
-                maxHeight   : 64
-            },
-            spacing         : {         // Add padding
-                padding     : 5
-            }
-        },
-        mode                : {
-            symbol          : true      // Activate the «symbol» mode
-        }
-    };
-
-
-gulp.task('svg', function() {
-    gulp.src('_assets/*.svg')
-    .pipe(svgSprite(config))
-    .pipe(gulp.dest('images'))
-    .pipe(gulp.dest('_includes'));
-
-});
-
-gulp.task('css', function() {
-    gulp.src('./css/style.scss')
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(autoprefixer('last 2 version', 'ie 9'))
-    .pipe(gulp.dest('./css'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest('./css'))
-    .pipe(gulp.dest('./_site/css'))
-    .pipe(browserSync.reload({stream:true}))
-    .pipe(notify({ message: 'Styles task complete' }));
-});
-
-// gulp.task('js', function() {
-//     gulp.src('./scripts/**.js')
-//     .pipe(concat('scripts.js'))
-//     .pipe(gulp.dest('js'))
-//     .pipe(rename({suffix: '.min'}))
-//     .pipe(uglify())
-//     .pipe(gulp.dest('js'))
-//     .pipe(gulp.dest('_site/js'))
-//     .pipe(notify({ message: 'Scripts task complete' }));
-// });
-
-gulp.task('critical-css', function() {
-    critical.generate({
-        // Your base directory
-        base: '_site/',
-        // HTML source file
-        src: 'index.html',
-        // CSS output file
-        dest: 'css/critical.min.css',
-        // Viewport width
-        width: 1200,
-        // Viewport height
-        height: 900,
-        // Minify critical-path CSS
-        minify: true
-    });
-});
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
 
 /**
  * Build the Jekyll Site
  */
 gulp.task('jekyll-build', function (done) {
-    browserSync.notify('Building Jekyll');
+    browserSync.notify(messages.jekyllBuild);
     return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
         .on('close', done);
 });
@@ -99,24 +27,40 @@ gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
 /**
  * Wait for jekyll-build, then launch the Server
  */
-gulp.task('browser-sync', ['jekyll-build'], function() {
+gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
     browserSync({
         server: {
             baseDir: '_site'
-        },
-        host: "localhost"
+        }
     });
 });
 
-gulp.task('watch', function() {
-  // Watch .scss files
-  gulp.watch('./_sass/**/*.scss', ['css']);
-  // Watch .js files
-  //gulp.watch('./scripts/**/*.js', ['js']);
-  // Watch .html files and posts
-  gulp.watch(['**/*.html', '*.md', '_posts/*'], ['jekyll-rebuild']);
+/**
+ * Compile files from _scss into both _site/css (for live injecting) and css (for future jekyll builds)
+ */
+gulp.task('sass', function () {
+    return gulp.src('./css/style.scss')
+        .pipe(sass({
+            includePaths: ['scss'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 2 version', 'ie 8'], { cascade: true }))
+        .pipe(gulp.dest('./_site/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('./css'));
 });
 
-gulp.task('default', function() {
-    gulp.start('css', 'browser-sync', 'watch');
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+    gulp.watch(['./_sass/*.scss', './css/*.scss'], ['sass']);
+    gulp.watch(['index.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
